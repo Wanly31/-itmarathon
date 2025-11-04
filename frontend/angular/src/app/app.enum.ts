45c47d040c619d260c1b536a246a0e837e0449a3
Wanly31 <laggerfeed270@gmail.com>
2025-11-04 20:37:47 +0200

    feat: add delete button

--- frontend/angular/src/app/app.enum.ts
+++ frontend/angular/src/app/app.enum.ts
@@ -19,6 +19,7 @@ export const enum IconName {
   Close = 'close',
   Link = 'link',
   Info = 'info',
+  Delete = 'delete',
 }
 
 export const enum AriaLabel {
@@ -31,6 +32,7 @@ export const enum AriaLabel {
   Close = 'Close',
   ParticipantLink = 'Copy personal link',
   Info = 'Info',
+  Delete = 'Delete',
 }
 
 export const enum InputSidebarText {
@@ -120,6 +122,7 @@ export enum ToastMessage {
   PleaseCreateYourRoom = 'Please create your room first.',
   PleaseJoinTheRoom = 'Please join the room first',
   SuccessDrawNames = 'Success! All participants are matched.\nLet the gifting magic start!',
+  SuccessDeleteUser = 'User deleted successfully!',
 }
 
 export const enum InputType {
@@ -259,6 +262,10 @@ export const enum Endpoint {
   users = '/api/users',
   roomsDraw = '/api/rooms/draw',
 }
+
+export const EndpointHelper = {
+  DeleteUserById: (id: string) => `/api/users/${id}`,
+};
 
 export const enum NavigationLinkSegment {
   Join = 'join',
